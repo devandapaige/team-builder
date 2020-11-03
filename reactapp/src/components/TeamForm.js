1cd0cfc9eb57ec8c4e5c1867f1e4f6bbbe9c35bc
Amanda Nelson <amanda.gaylord@gmail.com>
2020-11-02 19:23:57 -0500

    MVP and Stretch Goal Complete

---
+++ reactapp/src/components/TeamForm.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+function TeamForm(props) {
+  const { addNewMember, editTeamMember, editMember } = props;
+  const [teamMember, setTeamMember] = useState({
+    name: "",
+    email: "",
+    role: "",
+  });
+
+  useEffect(() => {
+    setTeamMember(editMember);
+  }, [editMember]);
+  const handleChanges = (e) => {
+    setTeamMember({ ...teamMember, [e.target.name]: e.target.value });
+  };
+  const subitForm = (e) => {
+    //so it doesn't refresh the whole page
+    e.preventDefault();
+    if (editMember) {
+      editTeamMember(teamMember);
+    } else {
+      addNewMember(teamMember);
+    }
+    setTeamMember({
+      name: "",
+      email: "",
+      role: "",
+    });
+  };
+  return (
+    <div className="form">
+      <Form onSubmit={subitForm}>
+        <FormGroup>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            id="name"
+            placeholder="Full Name"
+            name="name"
+            value={teamMember.name}
+            onChange={handleChanges}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            placeholder="email@email.com"
+            name="email"
+            value={teamMember.email}
+            onChange={handleChanges}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="role">Project Role</Label>
+          <Input
+            type="text"
+            id="role"
+            placeholder="Full Stack Engineer"
+            name="role"
+            value={teamMember.role}
+            onChange={handleChanges}
+            required
+          />
+        </FormGroup>
+        <Button type="submit">
+          {!editMember ? "Add Team Member" : "Edit Team Member"}
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default TeamForm;
---
+++ reactapp/src/components/TeamMemberCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Card, CardText, CardBody, CardTitle, Button} from 'reactstrap';
+
+function TeamMemberCard(props) {
+const {teamMembers, selectEditMember} = props;
+const editTeamMember = (teammate) => {
+    selectEditMember(teammate);
+}
+  return (
+  <div className="tmCard">
+      {
+          teamMembers.map( teammate => (
+              <Card key={teammate.id}>
+                  <CardBody>
+                      <CardText>
+                          <CardTitle>{teammate.name}</CardTitle>
+                          <CardText>{teammate.email}</CardText>
+                          <CardText>{teammate.role}</CardText>
+                      </CardText>
+                      <Button color='info' onClick={() => editTeamMember(teammate)}>Edit</Button>
+                  </CardBody>
+              </Card>
+          ))
+      }
+  </div>
+  );
+}
+
+export default TeamMemberCard;
